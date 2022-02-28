5ae14025e75c359f92ba9417643450ade5b8f638
AnhMTV <anhmtv@newayict.com>
2022-02-28 18:37:31 +0700

    Update Talisman install URL from chrome store

--- src/lib/wallets/talisman-wallet/index.ts
+++ src/lib/wallets/talisman-wallet/index.ts
@@ -4,7 +4,7 @@ import logo from './TalismanLogo.svg';
 export class TalismanWallet extends BaseDotsamaWallet {
   extensionName = 'talisman';
   title = 'Talisman';
-  installUrl = 'https://app.talisman.xyz/spiritkeys';
+  installUrl = 'https://chrome.google.com/webstore/detail/talisman-wallet/fijngjgcjhjmmpcmkeiomlglpeiijkld';
   noExtensionMessage =
     'You can use any Polkadot compatible wallet but we recommend using SubWallet';
   logo = {
