2c2747e845bfaa90114fcb8c774255fbabd3d945
Nam Phạm <namphamc99@gmail.com>
2022-08-24 11:20:38 +0700

    update logic wagmi-connect package

--- packages/wagmi-connect/src/index.ts
+++ packages/wagmi-connect/src/index.ts
@@ -1,3 +1,76 @@
 // Copyright 2017-2022 @subwallet/wagmi-connect authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import {
+  Chain,
+  ConnectorNotFoundError,
+  InjectedConnector,
+  ResourceUnavailableError,
+  RpcError,
+  UserRejectedRequestError
+} from '@wagmi/core';
+import { InjectedConnectorOptions } from '@wagmi/core/dist/declarations/src/connectors/injected';
+
+export interface WagmiSubConnectOptions extends InjectedConnectorOptions {
+
+}
+
+type WagmiConstructorParams = {
+  chains?: Chain[],
+  options?: WagmiSubConnectOptions
+}
+
+export class SubWalletConnector extends InjectedConnector {
+  override readonly id = 'subwallet';
+  override readonly ready = typeof window != 'undefined' && !!window.SubWallet;
+
+  constructor({chains, options: _options}: WagmiConstructorParams = {}) {
+    super({
+      chains,
+      options: {
+        name: 'SubWallet',
+        shimChainChangedDisconnect: true,
+        ..._options
+      }
+    });
+  }
+
+  override async connect({ chainId }: { chainId?: number } = {}): Promise<{ account: string; chain: { id: number; unsupported: boolean; }; provider: any; }> {
+    try {
+      const provider = await this.getProvider();
+      if (!provider) throw new ConnectorNotFoundError();
+
+      if (provider.on) {
+        provider.on('accountsChanged', this.onAccountsChanged);
+        provider.on('chainChanged', this.onChainChanged);
+        provider.on('disconnect', this.onDisconnect);
+      }
+
+      this.emit('message', { type: 'connecting' });
+
+      const account = await this.getAccount();
+      // Switch to chain if provided
+      let id = await this.getChainId();
+      let unsupported = this.isChainUnsupported(id);
+
+      if (chainId && id !== chainId) {
+        const chain = await this.switchChain(chainId);
+        id = chain.id;
+        unsupported = this.isChainUnsupported(id);
+      }
+
+      return { account, chain: { id, unsupported }, provider };
+    } catch (e) {
+      if (this.isUserRejectedRequestError(e)) throw new UserRejectedRequestError(e);
+      if ((<RpcError>e).code === -32002) throw new ResourceUnavailableError(e);
+
+      throw e;
+    }
+  }
+
+  override async getProvider() {
+    if (typeof window === 'undefined') return;
+
+    return window.SubWallet;
+  }
+}
